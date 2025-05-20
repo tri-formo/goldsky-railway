1f705417d11f024a92ec25d51254090d074c21cd
tri <tricao@formo.so>
2025-05-20 13:57:26 +0700

    log stack

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -66,6 +66,6 @@ export const errorHandler = (
   return res.status(500).json({
     status: "error",
     message: "Something went wrong",
-    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+    stack: err.stack,
   });
 };
